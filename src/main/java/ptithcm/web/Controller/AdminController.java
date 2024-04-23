7a300f74656358d375255a1a9b4030b3d7964e95
ktlolmn <thanhanhynh@gmail.com>
2024-04-23 19:34:44 +0700

    update list monan

--- src/main/java/ptithcm/web/Controller/AdminController.java
+++ src/main/java/ptithcm/web/Controller/AdminController.java
@@ -16,6 +16,7 @@ import ptithcm.web.Service.MonAnService;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Base64;
+import java.util.Collections;
 import java.util.List;
 
 @Controller
@@ -28,12 +29,12 @@ public class AdminController {
 	@GetMapping("/monan")
 	public String viewMonAn(Model model) {
 		List<MonAn> danhSachMonAn = monAnService.getAllMonAn();
+		Collections.sort(danhSachMonAn, (monAn1, monAn2) -> Boolean.compare(monAn2.getTrangThai(), monAn1.getTrangThai()));
 		for (MonAn monAn : danhSachMonAn) {
 				if (monAn.getImg() != null) {
 					String base64Image = Base64.getEncoder().encodeToString(monAn.getImg());
 					monAn.setBase64Image(base64Image);
 			}
-			System.out.print(monAn.getTrangThai());
 		}
 		model.addAttribute("danhSachMonAn", danhSachMonAn);
 		model.addAttribute("monAn", new MonAn());
