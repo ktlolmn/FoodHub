f8d54fc4e58069efd805ce9da68478020a8641de
ktlolmn <thanhanhynh@gmail.com>
2024-05-28 10:14:50 +0700

    interceptor for user

--- src/main/java/ptithcm/web/Interceptor/AuthenticationInterceptor.java
+++ src/main/java/ptithcm/web/Interceptor/AuthenticationInterceptor.java
@@ -1,27 +1,41 @@
 package ptithcm.web.Interceptor;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
+
 import java.io.IOException;
+import java.util.logging.Logger;
 
 @Component
 public class AuthenticationInterceptor implements HandlerInterceptor {
 
+    private static final Logger logger = Logger.getLogger(AuthenticationInterceptor.class.getName());
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
         String uri = request.getRequestURI();
-        if (uri.startsWith("/admin/") && !isAdmin(request)) {
+        logger.info("Requested URI: " + uri);
+
+        if (uri.startsWith("/admin/") && !isRole(request, "Admin")) {
+            logger.info("Non-admin user attempting to access /admin/*");
             response.sendRedirect("/login");
             return false;
         }
+
+        if (uri.startsWith("/user/") && !isRole(request, "User")) {
+            logger.info("Non-user role attempting to access /user/*");
+            response.sendRedirect("/login");
+            return false;
+        }
+
         return true;
     }
 
-    private boolean isAdmin(HttpServletRequest request) {
-        String role = (String) request.getSession().getAttribute("role");
-        return role != null && role.equals("Admin");
+    private boolean isRole(HttpServletRequest request, String role) {
+        String userRole = (String) request.getSession().getAttribute("role");
+        logger.info("User role: " + userRole);
+        return userRole != null && userRole.equals(role);
     }
-
 }
--- src/main/java/ptithcm/web/Interceptor/WebMvcConfig.java
+++ src/main/java/ptithcm/web/Interceptor/WebMvcConfig.java
@@ -13,6 +13,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(authenticationInterceptor).addPathPatterns("/admin/**");
+        registry.addInterceptor(authenticationInterceptor).addPathPatterns("/admin/**", "/user/**");
     }
 }
